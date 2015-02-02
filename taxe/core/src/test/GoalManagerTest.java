0bd135d7fb7ffa8b25a78fcaffd6e94543017b8f
sp1091 <sp1091@york.ac.uk>
2015-02-02 05:22:57 +0000

    Via Goal testing added but errors atm could be error in test, unsure

--- taxe/core/src/test/GoalManagerTest.java
+++ taxe/core/src/test/GoalManagerTest.java
@@ -1,5 +1,7 @@
 package test;
-
+/*
+ * Tests Goal completion and also via goal logic
+ */
 
 import gameLogic.Player;
 import gameLogic.PlayerManager;
@@ -9,12 +11,13 @@ import gameLogic.map.Position;
 import gameLogic.map.Station;
 import gameLogic.resource.ResourceManager;
 import gameLogic.resource.Train;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GoalManagerTest extends LibGdxTest {
     GoalManager gm;
@@ -37,22 +40,33 @@ public class GoalManagerTest extends LibGdxTest {
 
         Station station1 = new Station("station1","s1", new Position(5, 5));
         Station station2 = new Station("station2", "s2", new Position(2, 2));
-
-        Goal goal = new Goal(station1, station2, 0);
+        Station station3 = new Station("station3", "s3", new Position(2, 2));
+        
+        Goal goal = new Goal(station1, station2, station3,0,0);
         player1.addGoal(goal);
         player1.addResource(train);
 
         ArrayList<Station> route = new ArrayList<Station>();
         route.add(station1);
+        route.add(station3);
         route.add(station2);
+        
         train.setRoute(route);
 
         train.addHistory("station1", 0);
-
+       
         pm.turnOver();
         pm.turnOver();
-        train.addHistory("station2", 1);
-
+        
+        train.addHistory("station3", 1);
+       
+        assertFalse("Goal was completed in error", goal.isComplete(train));
+        
+        pm.turnOver();
+        pm.turnOver();
+        
+        train.addHistory("station2", 2);
+        
         ArrayList<String> completedStrings = gm.trainArrived(train, player1);
         assertTrue("Goal wasn't completed", goal.isComplete(train));
         assertTrue("Completed goal string not right", completedStrings.size() > 0);
