c0b2c68ec75a15cbd91403d606064b018563ce2c
hc969 <hc969@york.ac.uk>
2015-02-02 15:58:20 +0000

    Changed MainMenuScreen

--- taxe/core/src/fvs/taxe/MainMenuScreen.java
+++ taxe/core/src/fvs/taxe/MainMenuScreen.java
@@ -19,19 +19,20 @@ public class MainMenuScreen extends ScreenAdapter {
     Vector3 touchPoint;
     Texture mapTexture;
     Image mapImage;
+    
 
     public MainMenuScreen(TaxeGame game) {
         this.game = game;
         camera = new OrthographicCamera(TaxeGame.WIDTH, TaxeGame.HEIGHT);
         camera.setToOrtho(false);
 
-        playBounds = new Rectangle(TaxeGame.WIDTH/2 - 200, 350, 400, 100);
-        exitBounds = new Rectangle(TaxeGame.WIDTH/2 - 200, 200, 400, 100);
+        playBounds = new Rectangle(TaxeGame.WIDTH/2 - 0, 250, 150, 50);
+        exitBounds = new Rectangle(TaxeGame.WIDTH/2 - 200, 250, 150, 50);
         touchPoint = new Vector3();
-        mapTexture = new Texture(Gdx.files.internal("gamemap.png"));
+        mapTexture = new Texture(Gdx.files.internal("splash_screen.png"));
         mapImage = new Image(mapTexture);
     }
-
+    
     public void update() {
     	
         if (Gdx.input.justTouched()) {
@@ -53,24 +54,24 @@ public class MainMenuScreen extends ScreenAdapter {
         gl.glClearColor(1, 1, 1, 1);
         gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //Draw transparent map in the background
+        //Draw map in the background
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
         Color c = game.batch.getColor();
-        game.batch.setColor(c.r, c.g, c.b, (float) 0.3);
-        game.batch.draw(mapTexture, 0, 0);
+        game.batch.setColor(c.r, c.g, c.b, (float) 1.0);
+        game.batch.draw(mapTexture, -1, 0);
         game.batch.setColor(c);
         game.batch.end();
-
+    
+    
         //Draw rectangles, did not use TextButtons because it was easier not to
         game.shapeRenderer.setProjectionMatrix(camera.combined);
         game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        game.shapeRenderer.setColor(Color.GREEN);
+        game.shapeRenderer.setColor(Color.GRAY);
         game.shapeRenderer.rect(playBounds.getX(), playBounds.getY(), playBounds.getWidth(), playBounds.getHeight());
-        game.shapeRenderer.setColor(Color.RED);
+        game.shapeRenderer.setColor(Color.GRAY);
         game.shapeRenderer.rect(exitBounds.getX(), exitBounds.getY(), exitBounds.getWidth(), exitBounds.getHeight());
         game.shapeRenderer.end();
 
@@ -82,13 +83,14 @@ public class MainMenuScreen extends ScreenAdapter {
         String exitGameString = "Exit";
         game.font.draw(game.batch, exitGameString, exitBounds.getX() + exitBounds.getWidth()/2 - game.font.getBounds(exitGameString).width/2,
                 exitBounds.getY() + exitBounds.getHeight()/2 + game.font.getBounds(exitGameString).height/2); // centre the text
-
-        game.batch.end();
+        game.fontSmall.draw(game.batch, startGameString, playBounds.getX() + playBounds.getWidth()/2 - game.fontSmall.getBounds(startGameString).width/2,
+                playBounds.getY() + playBounds.getHeight()/2 + game.fontSmall.getBounds(startGameString).height/2); // center the text
     }
 
     @Override
     public void render(float delta) {
-        update();
+    	update();
         draw();
     }
+    
 }
